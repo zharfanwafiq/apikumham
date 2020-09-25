09da6a5657b6b5e8fe4bb2ea1848b355822eedad
zharfanwafiq <zharfanww@gmail.com>
2020-09-25 14:49:51 +0700

    setting routes di server js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
