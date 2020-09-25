608f3e3affed22b3034b1786ca655306d995d9e1
zharfanwafiq <zharfanww@gmail.com>
2020-09-25 14:14:47 +0700

    menambahkan file koneksi

---
+++ koneksi.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+// buat koneksi database
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'dbrestapi'
+});
+
+conn.connect((err) => {
+    if (err) throw err;
+    console.log('MySql Terkoneksi')
+});
+
+module.exports = conn;
